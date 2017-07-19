746c05eb586733f47eed2a90ba04b9ea563b3251
Benjamin Brink <tekbasse@yahoo.com>
2017-07-19 15:59:11 -0400

    adds api for scheduled parameters

--- tcl/email-inbound-procs.tcl
+++ tcl/email-inbound-procs.tcl
@@ -39,12 +39,14 @@ ad_proc acs_mail_lite::sched_parameters {
     -max_blob_chars:optional
     -mpri_min:optional
     -mpri_max:optional
-    -hpri_pkg_ids:optional
-    -lpri_pkg_ids:optional
+    -hpri_package_ids:optional
+    -lpri_package_ids:optional
     -hpri_party_ids:optional
     -lpri_party_ids:optional
     -hpri_subject_glob:optional
     -lpri_subject_glob:optional
+    -hpri_object_ids:optional
+    -lpri_object_ids:optional
 } {
     Returns a name value list of parameters used by ACS Mail Lite scheduled procs.
     If a parameter is passed with value, the value is assigned to parameter.
@@ -59,29 +61,139 @@ ad_proc acs_mail_lite::sched_parameters {
 
     @param max_blob_chars Any incoming email body part over this many characters is stored in a file instead of database. 
 
-    @
+    @param mpri_min Minimum threshold integer for medium priority. Below this is fast High priority.
+
+    @param mpri_max Maximum integer for medium priority. Above this is Low priority.
+
+    @param hpri_package_ids List of package_ids to process at fast/high priority.
+
+    @param lpri_package_ids List of package_ids to process at low priority.
+
+    @param hpri_party_ids List of party_ids to process at fast/high priority.
+
+    @param lpri_party_ids List of party_ids to process at low priority.
+    
+    @param hpri_subject_glob A glob searching subjects to flag as fast/high priority.
+
+    @param lpri_subject_glob A glob searching subjects to flag as low priority.   
+
+    @param hpri_object_ids List of object_ids to process at fast/high priority.
+
+    @param lpri_object_ids List of object_ids to process at low priority.
+
 } {
     # See one row table acs_mail_lite_ui
-
-    if { [db_table_exists acs_mail_lite_ui] } {
-    set sredpcs_override 0
-    set reprocess_old_p "f"
-    set max_concurrent 6
-    set max_blob_chars 32767
-        db_0or1row acs_mail_lite_ui_r {
-        select sredpcs_override,reprocess_old_p,max_concurrent
-        from acs_mail_lite_ui limit 1
-        }
-        db_dml acs_mail_lite_ui_w {
-        insert into acs_mail_lite_ui 
-        (sredpcs_override,reprocess_old_p,max_concurrent)
-            values (:sredpcs_override,:reprocess_old_p,:max_concurrent);
+    # sched_parameters sp
+    set sp_list [list \
+                     sredpcs_override \
+                     reprocess_old_p \
+                     max_concurrent \
+                     max_blob_chars \
+                     mpri_min \
+                     mpri_max \
+                     hpri_package_ids \
+                     lpri_package_ids \
+                     hpri_party_ids \
+                     lpri_party_ids \
+                     hpri_subject_glob \
+                     lpri_subject_glob \
+                     hpri_object_ids \
+                     lpri_object_ids ]
+    foreach sp $sp_list {
+        if { [info exists $sp] } {
+            set new(${sp}) [set $sp]
         }
     }
+    set changes_p [array exists new]
+    set exists_p [db_0or1row acs_mail_lite_ui_r {
+        select sredpcs_override,
+        reprocess_old_p,
+        max_concurrent,
+        max_blob_chars,
+        mpri_min,
+        mpri_max,
+        hpri_package_ids,
+        lpri_package_ids,
+        hpri_party_ids,
+        lpri_party_ids,
+        hpri_subject_glob,
+        lpri_subject_glob,
+        hpri_object_ids,
+        lpri_object_ids 
+        from acs_mail_lite_ui limit 1
+    } ]
 
+    if { $exists_p } {
+        if { $changes_p } {
+            foreach sp_n [array names new] {
+                set ${sp_n} $new($sp_n)
+            }
+        }
+    } else {
+        # set initial defaults
+        set sredpcs_override 0
+        set reprocess_old_p "f"
+        set max_concurrent 6
+        set max_blob_chars 32767
+        set mpri_min "999"
+        set mpri_max "99999"
+        set hpri_package_ids ""
+        set lpri_package_ids ""
+        set hpri_party_ids ""
+        set lpri_party_ids ""
+        set hpri_subject_glob ""
+        set lpri_subject_glob ""
+        set hpri_object_ids ""
+        set lpri_object_ids ""
+    }
 
-
-
+    if { !$exists_p || $changes_p } {
+        db_transaction {
+            if { $changes_p } {
+                db_dml acs_mail_lite_ui_d {
+                    delete from acs_mail_lite_ui
+                }
+            }
+            db_dml acs_mail_lite_ui_i {
+                insert into acs_mail_lite_ui 
+                (sredpcs_override,
+                 reprocess_old_p,
+                 max_concurrent,
+                 max_blob_chars,
+                 mpri_min,
+                 mpri_max,
+                 hpri_package_ids,
+                 lpri_package_ids,
+                 hpri_party_ids,
+                 lpri_party_ids,
+                 hpri_subject_glob,
+                 lpri_subject_glob,
+                 hpri_object_ids,
+                 lpri_object_ids)
+                values 
+                (:sredpcs_override,
+                 :reprocess_old_p,
+                 :max_concurrent,
+                 :max_blob_chars,
+                 :mpri_min,
+                 :mpri_max,
+                 :hpri_package_ids,
+                 :lpri_package_ids,
+                 :hpri_party_ids,
+                 :lpri_party_ids,
+                 :hpri_subject_glob,
+                 :lpri_subject_glob,
+                 :hpri_object_ids,
+                 :lpri_object_ids
+                 )
+            }
+        }
+    }
+    set s_list [list ]
+    foreach s $sp_list {
+        lappend s_list $s [set $s]
+    }
+    return $s_list
 }
 
 # Local variables:
