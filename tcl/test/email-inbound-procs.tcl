bc14bce997d1d45fe87df0146787c2b695f84464
Benjamin Brink <tekbasse@yahoo.com>
2017-08-27 21:33:26 -0400

    revise test to match changes in code

--- tcl/test/email-inbound-procs.tcl
+++ tcl/test/email-inbound-procs.tcl
@@ -212,8 +212,8 @@ aa_register_case -cats {api smoke} acs_mail_lite_inbound_procs_check {
                 # two vary in time, t1, t2
                 # two vary in size, s1, s2
 
-                set t0 [nsv_get acs_mail_lite scan_in_start_t_cs]
-                set dur_s [nsv_get acs_mail_lite scan_in_est_dur_p_cycle_s]
+                set t0 [nsv_get acs_mail_lite si_start_t_cs]
+                set dur_s [nsv_get acs_mail_lite si_dur_p_cycle_s]
                 set s0 [ns_config -int -set -min $su_max nssock_v4 maxinput $su_max]
                 aa_log "r161 given: t0 '${t0}' dur_s '${dur_s}' s0 '${s0}'"
 
